9efb4b8c861f7820579fa22867f6665ef77f771e
jlchen6 <lulei94@gmail.com>
2020-03-11 09:58:43 -0500

    Added js script file and added code to display current date

---
+++ script.js
@@ -0,0 +1,7 @@
+//Define variables
+
+
+//Display the current day
+$("#currentDay").text(moment().format('LL'));
+
+//When the page loads, timeblocks should be color coded according to whether those blocks are in the past/present/future
